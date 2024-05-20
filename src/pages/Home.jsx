f98e3a130e8bb4d1934bc29ac6668671dfd36ed8
ThinkMuk <b2000kang@gmail.com>
2024-05-20 20:15:31 +0900

    feat: solution implemented

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,36 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Card from '../components/Card';
+import styled from 'styled-components';
 
 const Home = () => {
-    return (
-        <> 
-            코드를 추가해주세요
-        </>
-    );
+  const WrapUsers = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    place-items: center;
+    margin-bottom: 10px;
+  `;
+  const [usersInfo, setUsersInfo] = useState('');
+  useEffect(() => {
+    axios
+      .get(`https://reqres.in/api/users?page=1&per_page=9`)
+      .then((res) => setUsersInfo(res.data.data))
+      .catch((e) => console.log(e));
+  }, []);
+  console.log(usersInfo);
+  return (
+    <>
+      <h2>User List</h2>
+      <WrapUsers>
+        {usersInfo &&
+          usersInfo.map((user) => (
+            <Card img={user.avatar} name={`${user.first_name} ${user.last_name}`} id={user.id} key={user.id} />
+          ))}
+      </WrapUsers>
+      <Link to='./Menu'>메뉴 페이지로 고고</Link>
+    </>
+  );
 };
 
 export default Home;
-
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,10 +1,26 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const User = () => {
+  const { userId } = useParams();
+  const [user, setUser] = useState('');
+  useEffect(() => {
+    axios
+      .get(`https://reqres.in/api/users/${userId}`)
+      .then((res) => setUser(res.data.data))
+      .catch((e) => console.log(e));
+  }, []);
+  return (
     <>
-        코드를 추가해주세요.
+      <h2>User Information</h2>
+      <img src={user.avatar} alt='profile' />
+      <h3>email : {user.email}</h3>
+      <h3>
+        {user.first_name} {user.last_name}
+      </h3>
     </>
+  );
 };
 
 export default User;
